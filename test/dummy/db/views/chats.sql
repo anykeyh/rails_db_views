eb467ce8a4901054df22c4edb78b20fb4a590fdf
Yacine Petitprez <yacine@kosmogo.fr>
2016-05-30 15:16:15 +0800

    Testing the string interpolation

--- test/dummy/db/views/chats.sql
+++ test/dummy/db/views/chats.sql
@@ -1,6 +1,6 @@
 # This is the request for virtual chat object.
 # !require messages
-SELECT tuple_sort(ARRAY[sender_id, receiver_id]) AS ids,
+SELECT #{"tuple_sort"}(ARRAY[sender_id, receiver_id]) AS ids,
   MIN(created_at) as created_at, MAX(updated_at) as updated_at, COUNT(*) as number_of_messages
 FROM messages
 GROUP BY tuple_sort(ARRAY[sender_id, receiver_id])
--- test/dummy/db/views/messages.sql
+++ test/dummy/db/views/messages.sql
@@ -2,7 +2,7 @@
 -- output:
 -- receiver_name, receiver_id, sender_name, sender_id, content, updated_at, created_at
 SELECT DISTINCT _.receiver_name, _.receiver_id, _.sender_name, _.sender_id, _.content, _.updated_at, _.created_at
-FROM
+FROM #{puts "This is evaluated in Ruby."; ""}
   (SELECT u2.name as receiver_name, u2.id as receiver_id,
       u1.name as sender_name, u1.id as sender_id,
       um.content as content, um.updated_at as updated_at, um.created_at as created_at
